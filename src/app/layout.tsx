1e09eb32d651d24fb5a6a545912b3502759a4281
City7g <City7gor@gmail.com>
2024-08-09 17:30:40 +0300

    Add button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import localFont from 'next/font/local'
-import './globals.css'
 import '../styles/main.scss'
 import Header from '@/components/Header'
 
---
+++ src/app/ui/buttons/page.tsx
@@ -0,0 +1,19 @@
+const buttons = {
+  'btn-primary': 'Primary',
+  'btn-secondary': 'Secondary',
+  'btn-tertiary': 'Tertiary',
+}
+
+export default function UIFonts() {
+  return (
+    <main>
+      <h1>Button</h1>
+
+      {Object.keys(buttons).map((key) => (
+        <button className={key} key={key}>
+          {buttons[key]}
+        </button>
+      ))}
+    </main>
+  )
+}
