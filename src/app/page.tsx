c8bf8ac062f73856da97d01249ffab46ce53de62
City7g <City7gor@gmail.com>
2024-08-10 13:10:40 +0300

    Add index about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import IndexAbout from '@/components/Index/About'
+
 export default function Home() {
   return (
     <main>
-      <h1 className="title-h1">Hello</h1>
+      <IndexAbout />
     </main>
   )
 }
