9c7e65c21810293e846efbe8024bcbf63d5db227
City7g <City7gor@gmail.com>
2024-08-10 18:32:02 +0300

    Fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,10 @@
+import Link from 'next/link'
 import FormRemind from './Form/Remind'
+import IconInstagram from './Icon/Socials/Instagram'
+import IconPinterest from './Icon/Socials/Pinterest'
+import IconTwitter from './Icon/Socials/Twitter'
+import IconFacebook from './Icon/Socials/Facebook'
+import IconTelegram from './Icon/Socials/Telegram'
 
 export default function Footer() {
   return (
@@ -14,12 +20,45 @@ export default function Footer() {
       </div>
 
       <div className="footer__column">
-        <h3 className="title-h5">Contact Us</h3>
+        <h3 className="title-h5 footer__title">Contact Us</h3>
+
+        <h4 className="text-caption footer__subtitle">Address</h4>
+        <p className="footer__link">15/4 Khreshchatyk Street, Kyiv</p>
+
+        <h4 className="text-caption footer__subtitle">Phone</h4>
+        <p className="footer__link">+380980099777</p>
+
+        <h4 className="text-caption footer__subtitle">General Enquiry:</h4>
+        <p className="footer__link">Kiev.Florist.Studio@gmail.com</p>
+
+        <h3 className="title-h5 footer__title">Follow Us</h3>
+
+        <div className="footer__socials">
+          <Link href="#">
+            <IconInstagram />
+          </Link>
+
+          <Link href="#">
+            <IconPinterest />
+          </Link>
+
+          <Link href="#">
+            <IconFacebook />
+          </Link>
+
+          <Link href="#">
+            <IconTwitter />
+          </Link>
+
+          <Link href="#">
+            <IconTelegram />
+          </Link>
+        </div>
       </div>
 
       <div className="footer__column">
-        <h3 className="title-h5">Shop</h3>
-        <ul>
+        <h3 className="title-h5 footer__title">Shop</h3>
+        <ul className="footer__list">
           <li>All Products</li>
           <li>Fresh Flowers</li>
           <li>Dried Flowers</li>
@@ -27,6 +66,27 @@ export default function Footer() {
           <li>Designer Vases</li>
           <li>Aroma Candles</li>
           <li>Freshener Diffuser</li>
+        </ul>
+
+        <h3 className="title-h5 footer__title">Service</h3>
+        <ul className="footer__list">
+          <li>Flower Subcription</li>
+          <li>Wedding & Event Decor</li>
+        </ul>
+      </div>
+
+      <div className="footer__column">
+        <h3 className="title-h5 footer__title">About Us</h3>
+
+        <ul className="footer__list">
+          <li>Our story</li>
+          <li>Blog</li>
+        </ul>
+
+        <ul className="footer__list">
+          <li>Shipping & returns</li>
+          <li>Terms & conditions</li>
+          <li>Privacy policy</li>
         </ul>
       </div>
     </footer>
