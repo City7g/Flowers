19aff2838dc46bedef09e575b969a8b88c39373c
City7g <City7gor@gmail.com>
2024-08-10 19:31:57 +0300

    Add menu dialog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import localFont from 'next/font/local'
 import '../styles/main.scss'
 import Header from '@/components/Header/Main'
-import PopupResetPassword from '@/components/Popup/ResetPassword'
+import DialogMenu from '@/components/Dialog/Menu'
 import Footer from '@/components/Footer'
 
 const gilroy = localFont({
@@ -46,8 +46,6 @@ export default function RootLayout({
         <Header />
 
         {children}
-
-        <Footer />
 
         {/* <PopupResetPassword /> */}
       </body>
